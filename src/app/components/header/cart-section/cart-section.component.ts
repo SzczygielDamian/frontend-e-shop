5b9b8e8ab572943c2e77809aa52b8c5097691489
DESKTOP-ARLO0UC\Tymek <szczygielek92@gmail.com>
2024-12-27 13:19:31 +0100

    update change detection strategy

--- src/app/components/header/cart-section/cart-section.component.ts
+++ src/app/components/header/cart-section/cart-section.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-cart-section',
   standalone: false,
-  
   templateUrl: './cart-section.component.html',
-  styleUrl: './cart-section.component.scss'
+  styleUrl: './cart-section.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CartSectionComponent {
 
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: false,
-  
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SidebarComponent {
   sidebarElements: string[] = ["Books", "Coffee Mugs", "Mouse Pads", "Luggage Tags"];
